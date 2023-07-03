4f727bf9e590ec843f0a27c14179f96e53bb8dd0
Marco André Steimbach <marco.steimbach@gmail.com>
2023-07-03 16:06:46 -0300

    Criação de HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { BrowserRouter as Router } from "react-router-dom"
 import { RoutesMain } from "../src/Router/RouterMain"
+import { GlobalStyle } from "./styles/globalStyle"
+import { ResetCSS } from "./styles/reset"
 
 function App() {
-
   return (
-    <>
+    <Router>
+      <GlobalStyle />
+      <ResetCSS />
       <RoutesMain />
-    </>
+    </Router>
   )
 }
 
--- src/Router/RouterMain.tsx
+++ src/Router/RouterMain.tsx
@@ -1,11 +1,13 @@
-import { Route, Routes } from 'react-router-dom'
-import { CadastroPage } from '../Pages/Cadastro'
+import { Route, Routes } from "react-router-dom"
+import { RegisterPage } from "../Pages/Cadastro"
+import { HomePage } from "../Pages/HomePage"
 
 
 export const RoutesMain = () => {
     return (
         <Routes>
-            <Route path='/Cadastro'> <CadastroPage /></Route>
+            <Route path="/Cadastro" element={<RegisterPage />} />
+            <Route path="/" element={<HomePage />} />
         </Routes >
     )
 }
