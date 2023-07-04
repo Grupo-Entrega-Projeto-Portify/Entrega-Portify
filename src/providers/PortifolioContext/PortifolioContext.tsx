8011a77cc03eec61e9643f5b39b481e43b6ae148
renanxr <renan.xr23@gmail.com>
2023-07-04 20:37:01 -0300

    atualização portfoliocontext

--- src/providers/PortifolioContext/PortifolioContext.tsx
+++ src/providers/PortifolioContext/PortifolioContext.tsx
@@ -1,14 +1,81 @@
-import { createContext, useState } from "react"
-import { IPortifolioContext, IPortifolioProviderProps } from "./types"
+import { createContext, useState, useEffect, useContext } from "react";
+import {
+	IPortfolio,
+	IPortifolioContext,
+	IPortifolioProviderProps,
+	IProject,
+	ICreatePortfolioInput,
+	IUpdatePortfolioInput,
+} from "./types";
+import { api } from "../../services/api";
+import { UserContext } from "../UserContext/UserContext";
 
-export const PortifolioContext = createContext({} as IPortifolioContext)
+export const PortifolioContext = createContext({} as IPortifolioContext);
 
 export const PortifolioProvider = ({ children }: IPortifolioProviderProps) => {
-    const [modalCreate, setModalCreate] = useState(false)
+	const [modalCreate, setModalCreate] = useState(false);
+	const [portfolios, setPortfolios] = useState<IPortfolio[]>([]);
+	const { user } = useContext(UserContext);
 
-    return (
-        <PortifolioContext.Provider value={{modalCreate, setModalCreate}}>
-            {children}
-        </PortifolioContext.Provider>
-    )
-}
+	useEffect(() => {
+		fetchPortfolios();
+	}, []);
+
+	const fetchPortfolios = async () => {
+		try {
+			const response = await api.get(
+				`/portfolios?_embed=projectsprojects&userId=${user.id}`
+			);
+			setPortfolios(response.data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const createPortfolio = async (portfolioData: ICreatePortfolioInput) => {
+		try {
+			const token = localStorage.getItem("@TOKEN");
+			const response = await api.post("/portfolios", portfolioData, {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			});
+			const createdPortfolio = response.data;
+			setPortfolios((prevPortfolios) => [...prevPortfolios, createdPortfolio]);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	const updatePortfolio = async (
+		portfolioId: number,
+		portfolioData: IUpdatePortfolioInput
+	) => {
+		try {
+			const token = localStorage.getItem("@TOKEN");
+			await api.put(`/portfolios/${portfolioId}`, portfolioData, {
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			});
+			setPortfolios((prevPortfolios) =>
+				prevPortfolios.map((portfolio) =>
+					portfolio.id === portfolioId
+						? { ...portfolio, ...portfolioData }
+						: portfolio
+				)
+			);
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	return (
+		<PortifolioContext.Provider
+			value={{ modalCreate, setModalCreate, portfolios, fetchPortfolios, createPortfolio,
+                updatePortfolio, }}
+		>
+			{children}
+		</PortifolioContext.Provider>
+	);
+};
