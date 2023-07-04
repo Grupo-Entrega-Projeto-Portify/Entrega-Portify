c82f71aa647647e550f6afde677026ecdfc01859
Marco André Steimbach <marco.steimbach@gmail.com>
2023-07-04 14:42:58 -0300

    Realizado criação de Name no Header Page User

--- src/components/UserPageComponents/Header/index.tsx
+++ src/components/UserPageComponents/Header/index.tsx
@@ -1,8 +1,20 @@
 
 import { HeaderUserPageCSS } from "./styled"
 import logo from "../../../assets/Logo.png"
+import { useEffect, useState } from "react"
 
 export const HeaderUserPage = () => {
+
+    const [userName, setUserName] = useState("")
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem("@USER")
+        if (storedUser) {
+            const user = JSON.parse(storedUser)
+            setUserName(user.name)
+        }
+    }, [])
+
     return (
         <>
             <HeaderUserPageCSS>
@@ -10,7 +22,7 @@ export const HeaderUserPage = () => {
                     <div className="header__div">
                         <img src={logo} alt="logo da Portify" />
                         <div className="header__divButton">
-                            <p className="header__userName">usuario</p>
+                            <p className="header__userName">{userName}</p>
                             <button className="header__button">Sair</button>
                         </div>
                     </div>
