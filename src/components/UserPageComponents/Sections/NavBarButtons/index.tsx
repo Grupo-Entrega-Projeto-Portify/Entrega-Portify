eccecb62a41a8aae5532f0a76abb93a43c6aaf10
Leoni <luianleoni@gmail.com>
2023-07-06 17:30:00 -0300

    style: navbar

--- src/components/UserPageComponents/Sections/NavBarButtons/index.tsx
+++ src/components/UserPageComponents/Sections/NavBarButtons/index.tsx
@@ -1,6 +1,7 @@
 import { GoHome } from "react-icons/go"
 import { FiUser } from "react-icons/fi"
 import { MdOutlineBackupTable } from "react-icons/md"
+import { StyleNavBar } from "./styled"
 
 export const SectionNavButtonsUserPage = ({ setCurrentSection }) => {
     const handleButtonClick = (section) => {
@@ -8,19 +9,25 @@ export const SectionNavButtonsUserPage = ({ setCurrentSection }) => {
     }
 
     return (
-        <section>
-            <button onClick={() => handleButtonClick("start")}>
-                <GoHome />
+        <StyleNavBar>
+
+        <section className="navbar__section">
+            <div className="navbar__div">
+
+            <button className="navbar__button" onClick={() => handleButtonClick("start")}>
+                <GoHome size={20} className="icon" />
                 Início
             </button>
-            <button onClick={() => handleButtonClick("information")}>
-                <FiUser />
+            <button className="navbar__button"  onClick={() => handleButtonClick("information")}>
+                <FiUser size={20} className="icon" />
                 Informações de perfil
             </button>
-            <button onClick={() => handleButtonClick("projects")}>
-                <MdOutlineBackupTable />
+            <button  className="navbar__button"  onClick={() => handleButtonClick("projects")}>
+                <MdOutlineBackupTable size={20} className="icon" />
                 Meus projetos
             </button>
+            </div>
         </section>
+        </StyleNavBar>
     )
 }
