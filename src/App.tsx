0e528ceff95497bcfb147360b8e0c0074c7b7fe9
Marco André Steimbach <marco.steimbach@gmail.com>
2023-07-06 21:50:32 -0300

    Criação de projeto feita com sucesso

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import { GlobalStyle } from "./styles/globalStyle"
 import { ResetCSS } from "./styles/reset"
 import { UserProvider } from "./providers/UserContext/UserContext"
 import { PortifolioProvider } from "./providers/PortfolioContext/PortfolioContext"
+import { ProjectProvider } from "./providers/ProjectContext/ProjectContext"
 
 function App() {
-  return (
+    return (
       <Router>
         <UserProvider>
           <PortifolioProvider >
-            <GlobalStyle />
-            <ResetCSS />
-            <RoutesMain />
+            <ProjectProvider>
+              <GlobalStyle />
+              <ResetCSS />
+              <RoutesMain />
+            </ProjectProvider>
           </PortifolioProvider>
         </UserProvider>
       </Router>
-  )
-}
+    )
+  }
 
 export default App
